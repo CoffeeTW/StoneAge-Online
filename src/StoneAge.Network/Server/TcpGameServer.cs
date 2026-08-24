8fc48392895207cd888a1bcc9d18b2b505cd2cff
CoffeeTW <m0911182606@gmail.com>
2026-08-24 16:42:38 +0800

    chore: update server hello version

--- src/StoneAge.Network/Server/TcpGameServer.cs
+++ src/StoneAge.Network/Server/TcpGameServer.cs
@@ -42,7 +42,7 @@ public sealed class TcpGameServer(IClientPacketHandler packetHandler)
         try
         {
             await using var stream = client.GetStream();
-            var helloPayload = Encoding.UTF8.GetBytes("StoneAge Online v0.1-05");
+            var helloPayload = Encoding.UTF8.GetBytes("StoneAge Online v0.1-09");
             await stream.WriteAsync(PacketCodec.Encode(Opcode.Hello, helloPayload), cancellationToken);
 
             while (!cancellationToken.IsCancellationRequested)
