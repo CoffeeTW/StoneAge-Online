3ccb2c9354e3077f8b92001275003cdcfb0d9580
CoffeeTW <m0911182606@gmail.com>
2026-08-26 09:01:17 +0800

    refactor: pass client connection to packet handlers

--- src/StoneAge.Network/Server/IClientPacketHandler.cs
+++ src/StoneAge.Network/Server/IClientPacketHandler.cs
@@ -1,9 +1,8 @@
-using System.Net.Sockets;
 using StoneAge.Network.Protocol;
 
 namespace StoneAge.Network.Server;
 
 public interface IClientPacketHandler
 {
-    Task HandleAsync(GameSession session, PacketFrame packet, NetworkStream stream, CancellationToken cancellationToken);
+    Task HandleAsync(ClientConnection connection, PacketFrame packet, CancellationToken cancellationToken);
 }
