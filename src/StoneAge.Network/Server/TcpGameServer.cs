2c0120ac21f2ac25f525fe7a6d71229d8475673b
CoffeeTW <m0911182606@gmail.com>
2026-08-26 10:37:30 +0800

    chore: bump server hello to v0.1-18

--- src/StoneAge.Network/Server/TcpGameServer.cs
+++ src/StoneAge.Network/Server/TcpGameServer.cs
@@ -43,7 +43,7 @@ public sealed class TcpGameServer(IClientPacketHandler packetHandler)
 
         try
         {
-            var helloPayload = Encoding.UTF8.GetBytes("StoneAge Online v0.1-17");
+            var helloPayload = Encoding.UTF8.GetBytes("StoneAge Online v0.1-18");
             await connection.SendAsync(Opcode.Hello, helloPayload, cancellationToken);
 
             while (!cancellationToken.IsCancellationRequested)
