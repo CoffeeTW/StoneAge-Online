7c7d4ac523238722194ba6212246cef7b53b83f8
CoffeeTW <m0911182606@gmail.com>
2026-08-26 09:18:51 +0800

    refactor: support mixed ClientConnection handler migration

--- src/StoneAge.Network/Server/IClientPacketHandler.cs
+++ src/StoneAge.Network/Server/IClientPacketHandler.cs
@@ -5,7 +5,8 @@ namespace StoneAge.Network.Server;
 
 public interface IClientPacketHandler
 {
-    Task HandleAsync(GameSession session, PacketFrame packet, NetworkStream stream, CancellationToken cancellationToken);
+    Task HandleAsync(GameSession session, PacketFrame packet, NetworkStream stream, CancellationToken cancellationToken)
+        => HandleAsync(new ClientConnection(stream, session), packet, cancellationToken);
 
     Task HandleAsync(ClientConnection connection, PacketFrame packet, CancellationToken cancellationToken)
         => HandleAsync(connection.Session, packet, connection.Stream, cancellationToken);
